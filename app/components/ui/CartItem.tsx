bd9e7c08d1776a2812a34fb3f3e65f34b67d6964
leticialfc <leticia@barkyn.com>
2025-09-14 12:36:53 +0100

    feat: update CartItem component to use Trash2 icon and enhance layout for better responsiveness

--- app/components/ui/CartItem.tsx
+++ app/components/ui/CartItem.tsx
@@ -2,7 +2,7 @@ import { useCart } from "~/contexts/CartContext";
 import type { Product } from "~/services/api.server";
 import Button from "./Button";
 import QuantityPicker from "./QuantityPicker";
-import { Trash } from "lucide-react";
+import { Trash2 as Trash } from "lucide-react";
 import ProductImage from "./ProductImage";
 
 interface CartItem extends Product {
@@ -23,13 +23,12 @@ const CartItem = ({ item }: CartItemProps) => {
     return (
         <div className="flex space-x-4 pb-4 border-b">
             <ProductImage src={item.thumbnail} alt={item.title} size="small" />
-            <div className="flex flex-col items-baseline justify-between">
+            <div className="flex flex-col items-baseline justify-between w-full lg:w-fit">
                 <div>
                     <h3 className="font-semibold">{item.title}</h3>
                     <p className="text-gray-600">${item.price.toFixed(2)}</p>
                 </div>
-
-                <div className="flex items-center gap-2">
+                <div className="flex items-center w-full justify-between lg:justify-normal lg:space-x-2">
                     <QuantityPicker
                         quantity={item.quantity}
                         onQuantityChange={handleQuantityChange}
@@ -42,7 +41,7 @@ const CartItem = ({ item }: CartItemProps) => {
                         onClick={() => removeFromCart(item.id)}
                         aria-label={`Remove ${item.title} from cart`}
                     >
-                        <Trash className="h-4 w-4" />
+                        <Trash className="h-5 w-5" />
                     </Button>
                 </div>
             </div>
