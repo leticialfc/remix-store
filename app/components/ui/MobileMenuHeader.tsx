527dfe8910789154f6e59c10f69be329d67a7ffb
leticialfc <leticia@barkyn.com>
2025-09-14 22:37:06 +0100

    fix: adjust padding in MobileMenuHeader for improved layout consistency

--- app/components/ui/MobileMenuHeader.tsx
+++ app/components/ui/MobileMenuHeader.tsx
@@ -22,7 +22,7 @@ const MobileMenuHeader = ({ isOpen, onClose }: MobileMenuHeaderProps) => {
     }, [isOpen]);
 
     return (
-        <header className="border-b border-gray-500 px-4 py-2 lg:px-14">
+        <header className="border-b border-gray-500 px-4 py-3 lg:px-14">
             <div className="max-w-screen-2xl mx-auto">
                 <div className="w-full flex items-center justify-between">
                     <HeaderLogo text={"The Online Store".toLocaleUpperCase()} />
